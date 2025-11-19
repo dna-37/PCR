d50014dafced5dd8891aa4773c7fe2274aa3e5e0
dna-37 <dannamonzoni@icloud.com>
2025-11-19 01:23:24 +0000

    :)

--- Main.java
+++ Main.java
@@ -1,1 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+
+            int Brice;
+            Brice = 67;
+            int Alejandra = 4;
+            int BriceAndAlejandra = Brice + Alejandra;
+
+            System.out.println("Brice = " + Brice);
+            System.out.println("Alejandra = " + Alejandra);
+            System.out.println("Brice + Alejandra = " + BriceAndAlejandra);
+//  :3
+    }
+    //function/method
+   static void WhostheBrowner(){
+        System.out.println("WHO'S THE BROWNER");
+        System.out.println("WHO'S THE BROWNER");
+        System.out.println("WHO'S THE BROWNER");
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
